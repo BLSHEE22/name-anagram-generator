204b17823122c81802625d28147db414d326b881
Brendan Sheedy <60196992+BLSHEE22@users.noreply.github.com>
2026-02-07 18:51:53 -0500

    Update test_algo.py

--- test_algo.py
+++ test_algo.py
@@ -1,1 +1,12 @@
+from anagram.generator import generate_anagrams
+import random
+
+name = "Grace Hopper"
+
+results = generate_anagrams(name, max_words=3)
+
+sample_anagrams = random.sample(results, 10)
+
+for s in sample_anagrams:
+    print(s)
 
